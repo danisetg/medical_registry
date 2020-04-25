82ffb9f26bd8438358ac9643aef334510722666c
Daniset Gonzalez <danisetg@gmail.com>
2020-04-25 18:17:23 -0500

    added name to app

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(__dirname+'/dist/doki-medical-app'));
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/dist/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/doki-medical-app/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
